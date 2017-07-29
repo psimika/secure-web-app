f2a3a9e0ab4a6d0f09e5ea560c0ab07b03d4cebd
Michaella Neirou <psimika@gmail.com>
2017-07-29 20:26:39 +0300

    Add db tests for users and sessions

---
+++ petfind/postgres/sessions_test.go
@@ -0,0 +1,42 @@
+package postgres_test
+
+import (
+	"testing"
+
+	"github.com/psimika/secure-web-app/petfind"
+)
+
+func TestDeleteUserSession(t *testing.T) {
+	s := setup(t)
+	defer teardown(t, s)
+
+	githubID := int64(5)
+	u := &petfind.User{Name: "Jane Doe", GithubID: githubID}
+	if err := s.CreateUser(u); err != nil {
+		t.Fatalf("CreateUser failed: %v", err)
+	}
+
+	if err := s.CreateUserSession(&petfind.Session{ID: "foo", UserID: 1}); err != nil {
+		t.Fatalf("CreateUserSession failed: %v", err)
+	}
+
+	if err := s.DeleteUserSession("foo"); err != nil {
+		t.Fatalf("DeleteUserSession failed: %v", err)
+	}
+
+	_, err := s.GetUserBySessionID("foo")
+	if err != petfind.ErrNotFound {
+		t.Fatalf("GetUserBySessionID after deleting session returned %v, expected: %q", err, petfind.ErrNotFound)
+	}
+}
+
+func TestCreateUserSession_forUnknownUser(t *testing.T) {
+	s := setup(t)
+	defer teardown(t, s)
+
+	// Test that we cannot create a session for an unknown user.
+	err := s.CreateUserSession(&petfind.Session{ID: "foo", UserID: 1})
+	if err == nil {
+		t.Fatalf("CreateUserSession for unknown user returned %v, expected err", err)
+	}
+}
---
+++ petfind/postgres/users_test.go
@@ -0,0 +1,83 @@
+// +build db
+
+package postgres_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/psimika/secure-web-app/petfind"
+)
+
+func TestCreateUser(t *testing.T) {
+	s := setup(t)
+	defer teardown(t, s)
+
+	githubID := int64(5)
+	u := &petfind.User{Name: "Jane Doe", GithubID: githubID}
+	if err := s.CreateUser(u); err != nil {
+		t.Fatalf("CreateUser failed: %v", err)
+	}
+
+	user, err := s.GetUserByGithubID(githubID)
+	if err != nil {
+		t.Fatalf("GetUserByGithubID failed: %v", err)
+	}
+
+	// Ignore time field.
+	user.Added = time.Time{}
+
+	want := &petfind.User{ID: 1, Name: "Jane Doe", GithubID: githubID}
+	if got := user; !reflect.DeepEqual(got, want) {
+		t.Fatalf("GetUserByGithubID \nhave: %#v\nwant: %#v", got, want)
+	}
+}
+
+func TestGetUserByGithubID_notFound(t *testing.T) {
+	s := setup(t)
+	defer teardown(t, s)
+
+	_, err := s.GetUserByGithubID(0)
+	if err != petfind.ErrNotFound {
+		t.Fatalf("GetUserByGithubID for unknown githubID returned %v, expected: %q", err, petfind.ErrNotFound)
+	}
+}
+
+func TestGetUserBySessionID(t *testing.T) {
+	s := setup(t)
+	defer teardown(t, s)
+
+	githubID := int64(5)
+	u := &petfind.User{Name: "Jane Doe", GithubID: githubID}
+	if err := s.CreateUser(u); err != nil {
+		t.Fatalf("CreateUser failed: %v", err)
+	}
+
+	if err := s.CreateUserSession(&petfind.Session{ID: "foo", UserID: 1}); err != nil {
+		t.Fatalf("CreateUserSession failed: %v", err)
+	}
+
+	user, err := s.GetUserBySessionID("foo")
+	if err != nil {
+		t.Fatalf("GetUserBySessionID failed: %v", err)
+	}
+
+	// Ignore time field.
+	user.Added = time.Time{}
+
+	want := &petfind.User{ID: 1, Name: "Jane Doe", GithubID: githubID}
+	if got := user; !reflect.DeepEqual(got, want) {
+		t.Fatalf("GetUserBySessionID \nhave: %#v\nwant: %#v", got, want)
+	}
+}
+
+func TestGetUserBySessionID_notFound(t *testing.T) {
+	s := setup(t)
+	defer teardown(t, s)
+
+	_, err := s.GetUserBySessionID("foo")
+	if err != petfind.ErrNotFound {
+		t.Fatalf("GetUserBySessionID for unknown sessionID returned %v, expected: %q", err, petfind.ErrNotFound)
+	}
+}
