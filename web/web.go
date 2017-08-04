286551a55b1470900daf6220e55830517ad1ac3c
Michaella Neirou <psimika@gmail.com>
2017-08-04 23:27:20 +0300

    Remove unused state whitelist
    State token is now stored in session.

--- web/web.go
+++ web/web.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
-	"sync"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
@@ -44,15 +43,14 @@ func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // server is the application's HTTP server.
 type server struct {
-	handlers       http.Handler
-	mux            *http.ServeMux
-	store          petfind.Store
-	tmpl           *tmpl
-	github         *oauth2.Config
-	stateWhitelist *stateWhitelist
-	sessions       sessions.Store
-	sessionTTL     int
-	sessionMaxTTL  int
+	handlers      http.Handler
+	mux           *http.ServeMux
+	store         petfind.Store
+	tmpl          *tmpl
+	github        *oauth2.Config
+	sessions      sessions.Store
+	sessionTTL    int
+	sessionMaxTTL int
 }
 
 // tmpl contains the server's templates required to render its pages.
@@ -82,14 +80,13 @@ func NewServer(
 	}
 
 	s := &server{
-		mux:            http.NewServeMux(),
-		store:          store,
-		tmpl:           t,
-		github:         newGitHubOAuthConfig(githubID, githubSecret),
-		stateWhitelist: newStateWhitelist(),
-		sessions:       sessionStore,
-		sessionTTL:     sessionTTL,
-		sessionMaxTTL:  sessionMaxTTL,
+		mux:           http.NewServeMux(),
+		store:         store,
+		tmpl:          t,
+		github:        newGitHubOAuthConfig(githubID, githubSecret),
+		sessions:      sessionStore,
+		sessionTTL:    sessionTTL,
+		sessionMaxTTL: sessionMaxTTL,
 	}
 	s.handlers = gorillactx.ClearHandler(s.mux)
 	s.mux.Handle("/", handler(s.homeHandler))
@@ -153,36 +150,6 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Strict-Transport-Security", "max-age=86400; includeSubDomains")
 	}
 	s.handlers.ServeHTTP(w, r)
-}
-
-// stateWhitelist is used to store any generated oauth state token in memory.
-// Only the ones that exist in the whitelist will be accepted and once they are
-// used they are deleted in order to mitigate replay attacks.
-type stateWhitelist struct {
-	mu sync.Mutex
-	m  map[string]bool
-}
-
-func newStateWhitelist() *stateWhitelist {
-	return &stateWhitelist{m: make(map[string]bool)}
-}
-
-func (w *stateWhitelist) Put(key string) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	w.m[key] = true
-}
-
-func (w *stateWhitelist) Get(key string) bool {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	return w.m[key]
-}
-
-func (w *stateWhitelist) Delete(key string) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	delete(w.m, key)
 }
 
 func (s *server) homeHandler(w http.ResponseWriter, r *http.Request) *Error {
